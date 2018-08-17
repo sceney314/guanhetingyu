986ba3e3e19fa24727a3047df1460ccf9f7180e9
zhengzp <zhengzp@jiedaibao.com>
2018-08-17 21:08:39 +0800

    搭框架继续

--- provider/src/main/java/com/guanhetingyu/www/Test.java
+++
@@ -1,7 +0,0 @@
-package com.guanhetingyu.www;
-
-/**
- * Created by jdb on 2018/3/27.
- */
-public class Test {
-}
---
+++ provider/src/main/java/com/guanhetingyu/www/controller/TestContoller.java
@@ -0,0 +1,10 @@
+package com.guanhetingyu.www.controller;
+
+/**
+ * Copyright (C), 2015-2018, 人人行科技股份有限公司
+ * Author:   zhengzp
+ * Date:     2018/8/17 下午8:58
+ * Description: 测试
+ */
+public class TestContoller {
+}
